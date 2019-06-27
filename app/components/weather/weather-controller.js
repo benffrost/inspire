18661e10a7b59267ce9b08d751185ed823c3ad49
benffrost <benffrost@gmail.com>
2019-06-27 15:22:13 -0600

    weather functionality

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -3,7 +3,8 @@ import WeatherService from "./weather-service.js";
 var _weatherService = new WeatherService()
 
 function drawWeather() {
-	console.log("THE WEATHER MAN SAYS:", _weatherService.Weather)
+	//console.log("THE WEATHER MAN SAYS:", _weatherService.Weather)
+	document.querySelector("#weather").innerHTML = _weatherService.Weather.Template
 }
 
 export default class WeatherController {
--- app/models/weather.js
+++ app/models/weather.js
@@ -2,11 +2,16 @@ export default class Weather {
   constructor(data) {
     console.log('[RAW WEATHER API DATA]', data);
 
-    // HEY FUN FACT 
+    // HEY FUN FACT
     // Have you ever wanted to know the temperature measured in kelvin? That is what this data returns!
     // data.main.temp is the temperature in Kelvin
     // You should probably convert the temperature data to either F or C
+    this.icon = data.weather[0].icon
     this.city = data.name
-    this.kelvin = data.main.temp
+    this.celcius = Math.round(parseFloat(data.main.temp) - 273.15)
+  }
+
+  get Template() {
+    return `${this.city} weather: <img src="http://openweathermap.org/img/wn/${this.icon}.png" alt="weather icon"></img> ${this.celcius} Deg C`
   }
 }
